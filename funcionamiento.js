80ac3563b9c519ab7ed3930285dee2e9d107e1b2
mplac555 <marcelo.placencia@epn.edu.ec>
2023-06-02 21:18:44 -0500

    Comentarios y corrección de indentación en script

--- funcionamiento.js
+++ funcionamiento.js
@@ -1,3 +1,4 @@
+// URLS DE ÍCONOS UTILIZADOS EN EL DOCUMENTO
 const URL_HTML = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png";
 const URL_JS = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/800px-Javascript-shield.svg.png?20180912181046";
 const URL_CSS = "https://i.pinimg.com/originals/eb/7e/20/eb7e20e646f5b7ec9ed4f8f78a5dee8f.png";
@@ -5,6 +6,7 @@ const URL_Node = "https://icon-library.com/images/nodejs-icon/nodejs-icon-17.jpg
 const URL_MongoDB = "https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png";
 const URL_GenericUser = "https://cdn.icon-icons.com/icons2/1993/PNG/512/account_avatar_face_man_people_profile_user_icon_123197.png";
 
+// CONTROLES DE ELEMENTOS HTML
 var devsSection = document.getElementById("developers");
 var txtNombre = document.getElementById("txtName");
 var txtAvatarURL = document.getElementById("txtAvatarUrl");
@@ -15,21 +17,24 @@ var chkCSS = document.getElementById("chkCSS");
 var chkNode = document.getElementById("chkNode");
 var chkMongoDB = document.getElementById("chkMongoDB");
 
+// LISTA DE DESARROLLADORES (INICIAL)
 var devs = [{nombre: "Carla",
-                avatarURL: "https://i.pinimg.com/236x/6d/5e/38/6d5e38d19bf4c0c9554b1e6beab75952.jpg",
-                bio:"Hola, soy desarrolladora front-end desde hace 5 años...",
-                lenguajes:{html:true, js:true, css:true, node:false, mongo:false}},
-                {nombre: "Erick",
-                avatarURL: "https://i.pinimg.com/564x/e9/57/2a/e9572a70726980ed5445c02e1058760b.jpg",
-                bio:"Hi, I am back-end developer from ten years ago, and ...",
-                lenguajes:{html:true, js:true, css:false, node:true, mongo:true}},
-                {nombre: "Maria",
-                avatarURL: "https://i.pinimg.com/564x/ed/be/19/edbe19b1fd4866b2d458aaabf8c02073.jpg",
-                bio:"Hola, soy DevOps en Facebook ...",
-                lenguajes:{html:false, js:false, css:false, node:true, mongo:true}}];
+             avatarURL: "https://i.pinimg.com/236x/6d/5e/38/6d5e38d19bf4c0c9554b1e6beab75952.jpg",
+             bio:"Hola, soy desarrolladora front-end desde hace 5 años...",
+             lenguajes:{html:true, js:true, css:true, node:false, mongo:false}},
 
-ImprimirDevs();
+             {nombre: "Erick",
+             avatarURL: "https://i.pinimg.com/564x/e9/57/2a/e9572a70726980ed5445c02e1058760b.jpg",
+             bio:"Hi, I am back-end developer from ten years ago, and ...",
+             lenguajes:{html:true, js:true, css:false, node:true, mongo:true}},
+             
+             {nombre: "Maria",
+             avatarURL: "https://i.pinimg.com/564x/ed/be/19/edbe19b1fd4866b2d458aaabf8c02073.jpg",
+             bio:"Hola, soy DevOps en Facebook ...",
+             lenguajes:{html:false, js:false, css:false, node:true, mongo:true}}
+];
 
+// FUNCIONES
 function ValidarDatos() {
     if (txtNombre.value == "") {
         alert("ERROR! El campo de nombre es obligatorio");
@@ -37,19 +42,19 @@ function ValidarDatos() {
     }            
     return true;
 }
-
 function AgregarDev() {
     devs.push({nombre: txtNombre.value,
-                avatarURL: txtAvatarURL.value,
-                bio: txtBio.value,
-                lenguajes:{html:chkHTML.checked,
-                            js:chkJS.checked,
-                            css:chkCSS.checked,
-                            node:chkNode.checked,
-                            mongo:chkMongoDB.checked}});
-    if (devs[devs.length - 1].avatarURL == "") devs[devs.length - 1].avatarURL = URL_GenericUser;
+               avatarURL: txtAvatarURL.value,
+               bio: txtBio.value,
+               lenguajes:{html:chkHTML.checked,
+                          js:chkJS.checked,
+                          css:chkCSS.checked,
+                          node:chkNode.checked,
+                          mongo:chkMongoDB.checked}});
+    if (devs[devs.length - 1].avatarURL == "") {
+        devs[devs.length - 1].avatarURL = URL_GenericUser;
+    }
 }
-
 function LimpiarCampos() {
     txtNombre.value = "";
     txtAvatarURL.value = "";
@@ -60,7 +65,6 @@ function LimpiarCampos() {
     chkNode.checked = false;
     chkMongoDB.checked = false;
 }
-
 function ImprimirDevs() {
     let cadenaHTML = '';
     for (let dev of devs) {
@@ -76,10 +80,12 @@ function ImprimirDevs() {
         devsSection.innerHTML = cadenaHTML;
     }
 }
-
 function InsertarNuevoDev() {
     if (!ValidarDatos()) return;
     AgregarDev();
     LimpiarCampos();
     ImprimirDevs();
 }
+
+// EJECUCIÓN DE LA IMPRESIÓN DE LA LISTA INICIAL DE LOS 3 DESARROLLADORES
+ImprimirDevs();
